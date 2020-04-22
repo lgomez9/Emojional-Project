3cfbaa07a8784edf5fb411eeec7fd6aab0e376fc
Leonardo Gómez <gmezleo97@gmail.com>
2020-04-22 15:07:45 -0500

    Initial Commit

---
+++ Emojional Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional Project
+//
+//  Created by Leonardo Gómez on 4/22/20.
+//  Copyright © 2020 Leonardo Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
