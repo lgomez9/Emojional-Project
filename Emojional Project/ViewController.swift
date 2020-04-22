58bd0bd63af4a0ed0113a28a3f3709ed0864ed63
Leonardo Gómez <gmezleo97@gmail.com>
2020-04-22 15:58:44 -0500

    Complete Iteration 3

--- Emojional Project/ViewController.swift
+++ Emojional Project/ViewController.swift
@@ -12,6 +12,12 @@ import Foundation
 class ViewController: UIViewController {
     
     let emojis = ["🙂" : "happy", "😘" : "lovey", "😭" : "crying", "😡" : "angry"]
+    let customMessages = [
+        "happy" : ["Glad you're feeling good!", "Way to go!", "Keep the good times rolling!"],
+        "lovey" : ["Aww, I love you too!", "Right back at you 😘", "I love you so much!"],
+        "crying" : ["It'll get better I promise!", "Tell me what's wrong.", "I hope you feel better soon!"],
+        "angry" : ["Try to let it go!", "Deep breaths... 3... 2... 1...", "Turn that frown upside-down!"]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +26,16 @@ class ViewController: UIViewController {
 
     @IBAction func showMessage(sender: UIButton) {
         
-        let selectedEmotion = sender.titleLabel?.text
+        let selectedEmotion = (sender.titleLabel?.text)!
+        let options = customMessages[emojis[selectedEmotion]!]!
+        let emojiMessage = options[Int.random(in: 0 ..< options.count)]
+        
+        
         
         // Figure out which UI Button was clicked
         // Send the message associated with that button
         
-        let alertController = UIAlertController(title: selectedEmotion!, message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: selectedEmotion, message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
